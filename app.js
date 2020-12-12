361769bcae8a4817dc590f6068a1b54dcad7af2b
drinith <felpmel@gmail.com>
2020-12-12 18:51:18 -0300

    primeiro

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+
+
+//Configurações
+//connect to mongodb
+
+
+app.get('/', (req, res) => {
+    res.send('hello')
+});
+
+app.listen(process.env.NODE_PORT, () => {
+    console.log(`Server started on port`);
+});
