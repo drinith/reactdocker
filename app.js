e3f5ed91e204bb8424c385777ed7ba8121b8b5d4
drinith <felpmel@gmail.com>
2020-12-13 23:30:30 -0300

    material do livro

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const app = express()
-
-
+const express = require("express");
+const app = express();
 
 //Configurações
 //connect to mongodb
 
-
-app.get('/', (req, res) => {
-    res.send('hello')
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
 app.listen(process.env.NODE_PORT, () => {
-    console.log(`Server started on port`);
+  console.log(`Server started on port`);
 });
