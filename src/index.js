bcf2f87aea48e1a9cc25341e45042292a507d0be
Your Name <you@example.com>
2020-12-14 17:21:34 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 // index.js
 // This is the main entry point of our application
+const express = require("express");
+const app = express();
+
+
+const port = process.env.NODE_PORT || 3000;
+
+//Configurações
+//connect to mongodb
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at ${process.env.NODE_IP}:${port}`)
+});
