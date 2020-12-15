5c84e38d8981d349ff112af26c6623a506776442
Your Name <you@example.com>
2020-12-15 17:06:39 -0300

    Capítulo 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@
 // This is the main entry point of our application
 const express = require("express");
 const app = express();
+const { ApolloServer,gql} = require('apollo-server-express');
+
+
+//Contruct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query{
+    hello:String 
+  }
+  `;
+
+  //Provide resolver functions for our schema fields
+  const resolvers = {
+    Query:{
+      hello:() => 'Hello world!'
+    }
+  } ;
+
+//Apollo Server setup
+const server = new ApolloServer({typeDefs,resolvers});
+
+//Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({app,path:'/api'});
 
 
 const port = process.env.NODE_PORT || 3000;
